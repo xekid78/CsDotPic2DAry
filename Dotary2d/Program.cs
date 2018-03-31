127a30c666ba93a8d2cf9fbf4ae739c28460775b
Makoto Tanabe <xekid78@outlook.com>
2018-03-31 15:03:40 +0900

    Add files via upload

---
+++ Dotary2d/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Dotary2d
+{
+    class Dotary2d
+    {
+        static void Main(string[] args)
+        {
+            int[][] letterA = 
+            {
+                new[] {0, 0, 1, 1, 0, 0},
+                new[] {0, 1, 0, 0, 1, 0},
+                new[] {1, 0, 0, 0, 0, 1},
+                new[] {1, 1, 1, 1, 1, 1},
+                new[] {1, 0, 0, 0, 0, 1},
+                new[] {1, 0, 0, 0, 0, 1}
+            };
+
+
+            foreach (int[] line in letterA)
+            {
+                foreach (int dot in line)
+                {
+                    if (dot == 1)
+                    {
+                        Console.Write("#");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
